57ba56b80c5aaa7214ce2f694d83d7309ef5fa6f
loveAtCorner <leo4817@yeah.net>
2024-10-21 17:10:41 +0800

    Update classifer.py

--- text_match/classifer.py
+++ text_match/classifer.py
@@ -1,9 +1,12 @@
 import os
 import re
 
-def remove_punctuation(word):
+"""
+分类，一条文本只有一个分类
+"""
+# def remove_punctuation(word):
     # `\W` 表示匹配任何非单词字符，等价于 `[^a-zA-Z0-9_]`。
-    return re.sub(r'^\W+|\W+$', '', word)
+    # return re.sub(r'^\W+|\W+$', '', word)
 
 def initialize_regexp_lists(directory):
     regexp_lists = {}
@@ -17,17 +20,15 @@ def initialize_regexp_lists(directory):
 
 def match_text_with_regex(input_text, regex_patterns):
     match_results = {}
-    input_text = remove_punctuation(input_text)
+    # input_text = remove_punctuation(input_text)
     for category, patterns in regex_patterns.items():
         matched_patterns = []
         for pattern in patterns:
-            if re.fullmatch(pattern, input_text):
+            if re.search(pattern, input_text):
                 matched_patterns.append(pattern)
-                # break  # 如果匹配到，则跳出循环，不再检查该分类的其他模式
         if matched_patterns:
             match_results[category] = matched_patterns
     return match_results
-
 
 
 if __name__ == "__main__":
