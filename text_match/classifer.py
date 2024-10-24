8e94ad5543e3290ea9f931c44054c536aa97fe98
loveAtCorner <leo4817@yeah.net>
2024-10-24 18:06:41 +0800

    Update classifer.py

--- text_match/classifer.py
+++ text_match/classifer.py
@@ -4,9 +4,14 @@ import re
 """
 分类，一条文本只有一个分类
 """
-# def remove_punctuation(word):
-    # `\W` 表示匹配任何非单词字符，等价于 `[^a-zA-Z0-9_]`。
-    # return re.sub(r'^\W+|\W+$', '', word)
+
+# 定义需要移除的中英文标点符号
+# PUNCTUATION_LIST = r"[，。！？、《》：；“”‘’（）【】『』（）{}［］—…·,\.!?<>\[\]:;\"'()\{\}\-\—]"
+PUNCTUATION_LIST = r"[，。！？、：《》：；“”‘’【】『』{}［］—…·,\.!?<>\[\]:;\"'{}\-\—]"
+
+def remove_punctuation(word):
+    # 使用明确列出的标点符号进行替换
+    return re.sub(PUNCTUATION_LIST, '', word)
 
 def initialize_regexp_lists(directory):
     regexp_lists = {}
@@ -20,12 +25,13 @@ def initialize_regexp_lists(directory):
 
 def match_text_with_regex(input_text, regex_patterns):
     match_results = {}
-    # input_text = remove_punctuation(input_text)
+    input_text = remove_punctuation(input_text)  # 去除标点符号
     for category, patterns in regex_patterns.items():
         matched_patterns = []
         for pattern in patterns:
-            if re.search(pattern, input_text):
+            if re.fullmatch(pattern, input_text):
                 matched_patterns.append(pattern)
+                # break  # 如果匹配到，则跳出循环，不再检查该分类的其他模式
         if matched_patterns:
             match_results[category] = matched_patterns
     return match_results
